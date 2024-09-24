d0a65403f7b00165892a53968c7fefec1cbdd188
AhmadAbdAlRhman <112072240+AhmadAbdAlRhman@users.noreply.github.com>
2024-09-24 18:25:14 +0300

    save PDF and Image

--- controllers/products.js
+++ controllers/products.js
@@ -1,5 +1,5 @@
 const Products = require("../modules/products");
-const {Sequelize , Op} = require("sequelize");
+const { Sequelize, Op } = require("sequelize");
 module.exports.getProducts = async (_req, res, _next) => {
   await Products.findAll()
     .then((result) => {
@@ -10,12 +10,18 @@ module.exports.getProducts = async (_req, res, _next) => {
     });
 };
 module.exports.addProducts = async (req, res, _next) => {
-  const { name, Description, image, pdf } = req.body;
+  const { name, Description } = req.body;
+  const image = req.files.image
+    ? req.files.image[0].filename
+    : req.file.filename;
+  const pdf = req.files.pdf 
+    ? req.files.pdf[0].filename 
+    : req.file.filename;
   const product = new Products({
     name,
     Description,
-    image,
-    pdf,
+    Image: image,
+    pdf: pdf,   
   });
   await product
     .save()
@@ -27,12 +33,12 @@ module.exports.addProducts = async (req, res, _next) => {
     });
 };
 module.exports.updateProducts = async (req, res, _next) => {
-  const { name, Description, image, pdf } = req.body;
+  const { name, Description, image } = req.body;
   const product = new Products({
     name,
     Description,
     image,
-    pdf,
+    // pdf,
   });
   await product
     .update()
@@ -56,12 +62,14 @@ module.exports.deleteProducts = async (req, res, _next) => {
 module.exports.searchProducts = async (req, res, _next) => {
   const productName = req.params.nameProduct;
   await Products.findOne({
-    where: Sequelize.where(Sequelize.fn("LOWER", Sequelize.col("name")),{
-        [Op.like]:`%${productName.toLowerCase()}%`        
+    where: Sequelize.where(Sequelize.fn("LOWER", Sequelize.col("name")), {
+      [Op.like]: `%${productName.toLowerCase()}%`,
+    }),
+  })
+    .then((result) => {
+      res.status(200).json(result);
     })
-  }).then((result) => {
-    res.status(200).json(result);
-  }).catch((err)=>{
-    res.status(404).json(err);
-  });
+    .catch((err) => {
+      res.status(404).json(err);
+    });
 };
--- route/products.js
+++ route/products.js
@@ -1,14 +1,41 @@
 const express = require("express");
 const router = express.Router();
+const multer = require("multer");
+const path = require("path");
 const products = require("../controllers/products");
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: (req, file, cb) => {
+      const fileType = file.mimetype;
+      if (fileType.includes("image")) {
+        cb(null, "./public/images");
+      } else if (fileType.includes("pdf")) {
+        cb(null, "./public/Document");
+      }
+    },
+    filename: (req, file, cb) => {
+      cb(
+        null,
+        file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+      );
+    },
+  }),
+}).fields([
+  { name: "image", maxCount: 1 }, // Handle single image upload
+  { name: "pdf", maxCount: 1 }, // Handle single PDF upload
+]);
 //select * from products
-router.get("/AllProducts",products.getProducts);
+router.get("/AllProducts", products.getProducts);
 //insert into products
-router.post("/AddProducts",products.addProducts);
+router.post(
+  "/AddProducts",
+    upload,
+    products.addProducts
+);
 //delete from products
-router.post("/deleteProducts",products.deleteProducts);
-//update products 
-router.post("/updateProducts",products.updateProducts);
+router.post("/deleteProducts", products.deleteProducts);
+//update products
+router.post("/updateProducts", products.updateProducts);
 //select from where name = ""
-router.get("/searchProducts/:nameProduct",products.searchProducts);
+router.get("/searchProducts/:nameProduct", products.searchProducts);
 module.exports = router;
