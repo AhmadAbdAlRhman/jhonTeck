13740a23f98ba28085a66ebd1a07d1c8d565c68c
AhmadAbdAlRhman <112072240+AhmadAbdAlRhman@users.noreply.github.com>
2024-09-26 17:11:30 +0300

    😎😎😎

--- server.js
+++ server.js
@@ -5,13 +5,20 @@ require("./modules/products");
 const sequelize = require("./utils/database");
 const app = express();
 const proRoute = require("./route/products");
+const corsOption = {
+  origin: ["http://localhost:4784", "http://localhost:3001"],
+  methods: ["POST", "GET"],
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOption));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(proRoute);
 sequelize.sync()
 .then(()=>{
-    app.listen(3001);
-    console.log(`This is server is work at http://localhost:3001`);
+    app.listen(4784);
+    console.log(`This is server is work at http://localhost:4784`);
 }).catch((err)=>{
     console.error(err);
 })
